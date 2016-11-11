f467871dcf25882e5a9e0ac5cf30064515aa0f9e
Jose Eduardo Meireles <jemeireles@gmail.com>
2016-11-11 12:46:51 -0600

    some refactoring and update vignette

--- vignettes/introduction_to_casper.R
+++ vignettes/introduction_to_casper.R
@@ -9,11 +9,9 @@ library("casper")
 # Example spectral dataset in matrix format.
 spec_matrix_example[1:4, 1:3]
 
-# Note that this is NOT a spectra object.
-# You can verify this by asking what class `spec_example` is.
-class(spec_matrix_example)
+# Note that this is NOT a spectra object. You can verify this by either asking what class `spec_example` is or using casper's `is_spectra()` function.
 
-# An alternative is to use casper's `is_spectra()` function.
+class(spec_matrix_example)
 is_spectra(spec_matrix_example)
 
 ## ------------------------------------------------------------------------
@@ -36,15 +34,15 @@ rf[1:4, 1:3]
 wl = colnames(rf)
 
 # Check the result
-wl[1:6]
+wl[1:4]
 
 # (3) Create a vector with sample labels that match
 #     the reflectance matrix rows.
-#     In this case, use the first colum of spec_example
+#     In this case, use the first colum of spec_matrix_example
 sn = spec_matrix_example[ , 1] 
 
 # Check the result
-sn[1:6]
+sn[1:4]
 
 # Finally, construct the spectra object using the `spectra` constructor
 spec = spectra(reflectance = rf, wavelengths = wl, names = sn)
@@ -61,15 +59,15 @@ spec_as_mat[1:4, 1:3]
 par(mfrow = c(1, 3))
 
 # Simple spectra plot
-plot(spec, lwd = 0.75, lty = 1, col = "grey25")
+plot(spec, lwd = 0.75, lty = 1, col = "grey25", main = "All Spectra")
 
 # Stand along quantile plot
-plot_quantile(spec, total_prob = 0.8, 
+plot_quantile(spec, total_prob = 0.8, main = "80% spectral quantile", 
               col = rgb(1, 0, 0, 0.5), lwd = 0.5, border = TRUE)
 
 # Combined quantile and individual spctra plot
 # With an added bonus of shading 4 spectral regions
-plot(spec, lwd = 0.25, lty = 1, col = "grey50")
+plot(spec, lwd = 0.25, lty = 1, col = "grey50", "Spectra, quantile and regions")
 plot_quantile(spec, total_prob = 0.8, 
               col = rgb(1, 0, 0, 0.25), add = TRUE, border = FALSE)
 plot_spec_regions(spec, regions = default_spec_regions, add = TRUE)
@@ -87,32 +85,42 @@ w[1:5]
 # You can also get the dimensions of your `spectra` object
 dim(spec)
 
-## ---- error=TRUE, fig.height=3, fig.width=4, fig.align="center"----------
+## ---- fig.height=2.5, fig.width=6, fig.align="center"--------------------
 # Subset spectra to all entries where sample_name matches "species_8"
 spec_sp8 = spec[ "species_8", ]
+
+# Check the results
+dim(spec_sp8)
+
+# Plotting the seubset result should work just fine
+par(mfrow = c(1, 2), cex.main = 0.8, cex.axis = 0.6, cex.lab = 0.8)
+
+plot(spec_sp8, col = "red", main = "Species 8 spectra")
+plot_quantile(spec, total_prob = 1.0, add = TRUE,  col = rgb(0.2, 0.2, 0.2, 0.2), border = FALSE)
+plot_spec_regions(spec_sp8, default_spec_regions, col = rgb(1, 0.5, 0, 0.1), add = TRUE)
 
 # And maybe further subset to the visible wavelengths only
 spec_sp8 = spec_sp8[ , 400:700 ]
 
-dim(spec_sp8)
-
 # This subset should still plot just fine
-plot(spec_sp8, col = "red", main = "Visible spectra for species 8",
-     cex.main = 0.8, cex.axis = 0.75)
-
-plot_quantile(spec, total_prob = 1.0, add = TRUE, 
-              col = rgb(0.2, 0.2, 0.2, 0.2), border = FALSE)
+plot(spec_sp8, col = "red", main = "Visible spectra for species 8")
+plot_quantile(spec, total_prob = 1.0, add = TRUE,  col = rgb(0.2, 0.2, 0.2, 0.2), border = FALSE)
 
 ## ---- error=TRUE---------------------------------------------------------
-# Note that you can subset by wavelength using numerics or characters.
+# Subset samples by index should work. It is also OK to subset by wavelength 
+# using numerics or characters.
 reflectance(spec_sp8[ 1 , "405"]) == reflectance(spec_sp8[ 1 , 405])
 
-# But you CANNOT use indexes to subset wavelengths!
-# Something that is obvioulsy an index will fail. For instance, using 2 instead of 401
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+# But remember that you CANNOT use indexes to subset wavelengths!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+
+# Something that is obvioulsy an index, like using 2 instead of 401 (the 2nd band), will fail.
 spec_sp8[ , 2 ]
 
-# However, if you use 2000:2001 you will NOT get the two last bands, but instead
-# wavelengths "2000" and "2001".Bottomline, be careful not to use indexes!
+# However, `casper` canot detect if you meant to get the two last bands when
+# you use 2000:2001. It will assume that you wanted wavelengths "2000" and "2001"
+# Bottomline, be very careful not to use indexes to subset wavelengths!
 
 ## ------------------------------------------------------------------------
 spec_new = spec
