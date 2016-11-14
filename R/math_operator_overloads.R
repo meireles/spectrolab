be6a1497d08ecc7edacf89ca469c086fef1c9dc7
Jose Eduardo Meireles <jemeireles@gmail.com>
2016-11-13 19:50:58 -0600

    added operator overload

--- R/math_operator_overloads.R
+++ R/math_operator_overloads.R
@@ -1,39 +1,38 @@
-#' Title
+#' Operator overloading for spectra
+#'
+#' @param e1
+#' @param e2
 #'
 #' @return
 #' @export
-#'
-#' @examples
-`+.spectra` = function(){message("math_overload")}
+Ops.spectra = function(e1, e2){
+    w = c(class(e1), class(e2))
+    w = which(w == "spectra")
 
-#' Title
-#'
-#' @return
-#' @export
-#'
-#' @examples
-`-.spectra` = function(){message("math_overload")}
+    if(w == 1){
+        e1[] = do.call(.Generic, list( reflectance(e1), e2))
+        return(e1)
+    } else {
+        e2[] = do.call(.Generic, list( reflectance(e2), e1))
+        return(e2)
+    }
+}
 
-#' Title
+#' Matrix multiplication
+#'
+#' @param e1
+#' @param e2
 #'
 #' @return
 #' @export
-#'
-#' @examples
-`*.spectra` = function(){message("math_overload")}
-
-#' Title
-#'
-#' @return
-#' @export
-#'
-#' @examples
-`/.spectra` = function(){message("math_overload")}
-
-#' Title
-#'
-#' @return
-#' @export
-#'
-#' @examples
-`%*%.spectra` = function(){message("math_overload")}
+`%*%.spectra` = function(e1, e2){
+    stop("Not implemented")
+    # w = c(class(e1), class(e2))
+    # w = which(w == "spectra")
+    #
+    # if(w == 1){
+    #     reflectance(e1) %*% e2
+    # } else {
+    #     reflectance(e2) %*% e1
+    # }
+}
