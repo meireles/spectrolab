8cd47c2f47c1bdf6c7bfc89a3998413b10d0cc23
Jose Eduardo Meireles <jemeireles@gmail.com>
2016-11-14 10:37:30 -0600

    updates to math op overload

--- R/math_operator_overloads.R
+++ R/math_operator_overloads.R
@@ -5,63 +5,60 @@
 #'
 #' @return
 #' @export
-Ops.spectra = function(e1, e2){
+Ops.spectra = function(e1, e2) {
     mathop = c("+", "-", "*", "/", "^")
     boolop = c("==", "!=", "<", "<=", ">=", ">")
-    implop = c(mathop, boolop)
+
+    if( ! .Generic %in% c(mathop, boolop) ){ stop("Not implemented") }
 
     is_spec   = c(is_spectra(e1), is_spectra(e2))
     w_is_spec = which(is_spec)
     s_is_spec = length(w_is_spec) == 1L
 
-    if( .Generic %in% implop ) {
-        # single spectra object
-        if( s_is_spec ) {
-            # spectra is first
-            if( w_is_spec == 1 ){
-                if(.Generic %in% mathop){
-                    e1[] = do.call(.Generic, list( reflectance(e1), e2) )
-                    return(e1)
-                }
-                if(.Generic %in% boolop){
-                    return(do.call(.Generic, list( reflectance(e1), e2) ))
-                }
-            }
-            # spectra is second
-            if( w_is_spec == 2 ) {
-                if(.Generic %in% mathop){
-                    e2[] = do.call(.Generic, list( e1, reflectance(e2)) )
-                    return(e2)
-                }
-                if(.Generic %in% boolop){
-                    return(do.call(.Generic, list( e1, reflectance(e2)) ))
-                }
-            }
-
-        # both are spectra
-        } else {
-            if( any(dim(e1) != dim(e2)) ){
-                stop("incompatible spectra dimensions")
-            }
-            if( any(wavelengths(e1) != wavelengths(e2)) ) {
-                stop("wavelength labels must be identical")
-            }
+    # single spectra object
+    if( s_is_spec ) {
+        # spectra is first
+        if( w_is_spec == 1 ){
             if(.Generic %in% mathop){
-                e1[] = do.call(.Generic, list(reflectance(e1), reflectance(e2)) )
-                if(any(names(e1) != names(e2))){
-                    warning("sample names not identical: removing sample names...")
-                    names(e1) = rep(NA, dim(e1)["n_samples"])
-                }
+                e1[] = do.call(.Generic, list( reflectance(e1), e2) )
                 return(e1)
             }
             if(.Generic %in% boolop){
-                return(do.call(.Generic, list(reflectance(e1), reflectance(e2)) ))
+                return(do.call(.Generic, list( reflectance(e1), e2) ))
             }
         }
+        # spectra is second
+        if(w_is_spec == 2) {
+            if(.Generic %in% mathop){
+                e2[] = do.call(.Generic, list( e1, reflectance(e2)) )
+                return(e2)
+            }
+            if(.Generic %in% boolop){
+                return(do.call(.Generic, list( e1, reflectance(e2)) ))
+            }
+        }
+    # both arguments are spectra
     } else {
-        stop("Not implemented")
+        if( any(dim(e1) != dim(e2)) ){
+            stop("incompatible spectra dimensions")
+        }
+        if( any(wavelengths(e1) != wavelengths(e2)) ) {
+            stop("wavelength labels must be identical")
+        }
+        if(.Generic %in% mathop){
+            e1[] = do.call(.Generic, list(reflectance(e1), reflectance(e2)) )
+            if(any(names(e1) != names(e2))){
+                warning("sample names not identical: removing sample names...")
+                names(e1) = rep(NA, dim(e1)["n_samples"])
+            }
+            return(e1)
+        }
+        if(.Generic %in% boolop){
+            return(do.call(.Generic, list(reflectance(e1), reflectance(e2)) ))
+        }
     }
 }
+
 
 #' Matrix multiplication
 #'
