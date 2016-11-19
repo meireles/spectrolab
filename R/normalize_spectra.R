96eee618861428e27e2f12b3560ac70e74ee8988
Jose Eduardo Meireles <jemeireles@gmail.com>
2016-11-19 10:38:23 -0600

    updated docs for normalize spectra

--- R/normalize_spectra.R
+++ R/normalize_spectra.R
@@ -1,27 +1,29 @@
-#' Vector normalization of spectra
+#' Vector normalize spectra
 #'
-#' @param spec spectra object to be vector normalized
+#' \code{normalize_spectra} returns a spectra obj with vector normalized reflectances
+#'
+#' @param x spectra object to be vector normalized
 #'
 #' @return spectra object with normalized spectra
 #' @export
-normalize_spectra = function(spec){
+normalize_spectra = function(x){
 
-    if( !is_spectra(spec) ){
+    if( !is_spectra(x) ){
         stop("Object must be of class spectra")
     }
 
-    refl            = reflectance(spec)
+    refl            = reflectance(x)
     refl_squared    = refl * refl
     vec_ones        = rep.int(1L, ncol(refl_squared))
     spec_sq_rowsum  = refl_squared %*% vec_ones
     magnitudes      = as.vector(sqrt(spec_sq_rowsum))
 
     # normalize and construct a `spectra` object
-    spec[] = i_reflectance(refl / magnitudes)
+    x[] = i_reflectance(refl / magnitudes)
 
     # add a magnitute attribute to the`spectra` object
     # spec$magnitudes = magnitudes
 
     # return
-    spec
+    x
 }
