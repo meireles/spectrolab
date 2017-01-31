1524318fc7e48a77c6844a1829ab68778d9e9ae2
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-01-30 22:09:20 -0600

    workong on overlap match

--- data-raw/estimate_overlap_matching_params.R
+++ data-raw/estimate_overlap_matching_params.R
@@ -1,12 +1,19 @@
 library("spectrolab")
-library("distr")
 
-path_input = "data-raw/svc_raw_and_overlap_matched/"
-spectra    = read_spectra(path_input, "sig")
+path_input  = "data-raw/svc_raw_and_overlap_matched"
+spectra     = read_spectra(path_input, "sig")
 
-spectra$`994`[ spectra$`994` < 0.8 ]
+spectra_moc = spectra$`994`
+spectra_raw = spectra$`1024`
 
-f = apply(reflectance(spectra$`994`), 1, function(x){all(x > 0.8)})
+all_greater_than = function(s, m){
+    apply(reflectance(s), 1, function(x){ all(x > m) })
+}
 
-p = normalize(spectra$`994`[ f, ])
+spectra_moc = spectra_moc[ ! all_greater_than(spectra_moc, 0.8), ]
+spectra_raw = spectra_raw[ ! all_greater_than(spectra_raw, 0.8), ]
+
+
+plot(spectra_moc, xlim = c(920, 1080))
+plot(spectra_raw, add = T, col = "red")
 
