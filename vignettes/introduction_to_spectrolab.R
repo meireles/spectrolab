09184f8b4d1304703a8e4ed51dc9e0ba6f9853b4
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-02-01 09:52:30 -0600

    more updates to the intro vignette

--- vignettes/introduction_to_spectrolab.R
+++ vignettes/introduction_to_spectrolab.R
@@ -6,32 +6,25 @@
 library("spectrolab")
 
 ## ---- eval=TRUE----------------------------------------------------------
-dir_path <- system.file("extdata/spec_matrix_meta.csv", package = "spectrolab")
-spec_csv <- read.csv(dir_path, check.names = F)
+dir_path = system.file("extdata/spec_matrix_meta.csv", package = "spectrolab")
+spec_csv = read.csv(dir_path, check.names = F)
 
-## The sample names are in column 3
-## Columns 1 and 2 are metadata
-achillea_spec <- as.spectra(spec_csv, name_idx = 3, meta_idxs = c(1,2) )
+# The sample names are in column 3. Columns 1 and 2 are metadata
+achillea_spec = as.spectra(spec_csv, name_idx = 3, meta_idxs = c(1,2) )
 
-## And now you have a spectra object with sample names and metadata...
+# And now you have a spectra object with sample names and metadata...
 achillea_spec
 
 ## ---- eval=TRUE----------------------------------------------------------
 # `dir_path` is the directory where our example datasets live
-dir_path <- system.file("extdata", "Acer_example", package = "spectrolab")
+dir_path = system.file("extdata", "Acer_example", package = "spectrolab")
 
 # Read .sig files
-acer_spectra <- read_spectra(path = dir_path, format = "sig")
+acer_spectra = read_spectra(path = dir_path, format = "sig")
 
 ## ---- eval=TRUE----------------------------------------------------------
-# Check the filenames to see if there are any flags
-names(acer_spectra)
-
 # use the `exclude_if_matches` argument to excluded flagged files
-acer_spectra <- read_spectra(path = dir_path, format = "sig", exclude_if_matches = c("BAD","WR"))
-
-# and check result
-plot(acer_spectra)
+acer_spectra = read_spectra(path = dir_path, format = "sig", exclude_if_matches = c("BAD","WR"))
 
 ## ---- eval=TRUE----------------------------------------------------------
 # Simply print the object
@@ -42,25 +35,28 @@ dim(acer_spectra)
 
 ## ---- eval=TRUE----------------------------------------------------------
 # Vector of all sample names. Note: Duplicated sample names are permitted
-n <- names(achillea_spec)
+n = names(achillea_spec)
 
 # Vector of wavelengths
-w <- wavelengths(achillea_spec)
+w = wavelengths(achillea_spec)
 
 # Reflectance matrix
-r <- reflectance(achillea_spec)
+r = reflectance(achillea_spec)
 
 # Metadata. Use simplify = TRUE to get a vector instead of a data.frame
 m = meta(achillea_spec, "ssp", simplify = TRUE)
 
 ## ---- eval=TRUE----------------------------------------------------------
-# Subset wavelength regions 
-spec_sub_vis <- achillea_spec[ , 400:700]
+# Subset wavelength regions.
+spec_sub_vis = achillea_spec[ , 400:700 ]
 
 # Subset spectra to all entries where sample_name matches "ACHMI_7" or
 # get the forst three samples
-spec_sub_byname <- achillea_spec["ACHMI_7", ]
-spec_sub_byidx  <- achillea_spec[ 1:3, ]
+spec_sub_byname = achillea_spec["ACHMI_7", ]
+spec_sub_byidx  = achillea_spec[ 1:3, ]
+
+## ---- eval=TRUE----------------------------------------------------------
+acer_spectra_trim = acer_spectra[ , wavelengths(acer_spectra, 400, 2400) ]
 
 ## ---- eval=TRUE----------------------------------------------------------
 # Subsetting samples by indexes works and so does subsetting wavelengths by numerics or characters.
@@ -88,26 +84,26 @@ plot_quantile(achillea_spec, total_prob = 0.8, col = rgb(1, 0, 0, 0.25), border 
 plot_regions(achillea_spec, regions = default_spec_regions(), add = TRUE)
 
 ## ---- eval=T-------------------------------------------------------------
-spec_new <- achillea_spec
+spec_new = achillea_spec
 
 # Replace names with a lowercase version
-names(spec_new) <- tolower(names(achillea_spec))
+names(spec_new) = tolower(names(achillea_spec))
 
 # Check the results
 names(spec_new)[1:5]
 
-## ----  fig.height=3, fig.width=4, fig.align="center", eval=F-------------
-#  # Scale reflectance by 0.75
-#  spec_new <- spec_new * 0.75
-#  
-#  # Plot the results
-#  plot(spec, col = "blue", lwd = 0.75, cex.axis = 0.75)
-#  plot(spec_new, col = "orange", lwd = 0.75, add = TRUE)
+## ----  fig.height=3, fig.width=4, fig.align="center", eval=T-------------
+# Scale reflectance by 0.75
+spec_new = spec_new * 0.75
+
+# Plot the results
+plot(achillea_spec, col = "blue", lwd = 0.75, cex.axis = 0.75)
+plot(spec_new, col = "orange", lwd = 0.75, add = TRUE)
 
 ## ---- eval = TRUE--------------------------------------------------------
 ## Adding metadata to a spectra object: a dummy N content
-n_content <- rnorm(n = nrow(achillea_spec), mean = 2, sd = 0.5)
-meta(achillea_spec, label = "N_percent") <- n_content
+n_content = rnorm(n = nrow(achillea_spec), mean = 2, sd = 0.5)
+meta(achillea_spec, label = "N_percent") = n_content
 
 ## ---- eval=T-------------------------------------------------------------
 # Make a matrix from a `spectra` object
@@ -116,5 +112,5 @@ spec_as_mat[1:4, 1:3]
 
 # Make a matrix from a `spectra` object
 spec_as_df = as.data.frame(achillea_spec, fix_names = "none")
-spec_as_df[1:4, 1:3]
+spec_as_df[1:4, 1:5]
 
