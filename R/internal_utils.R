meireles/spectrolab

commit d595e62a63a67e2cd08bdf751bbfb65bcd3488ce

fixed bug in subset where samples were not being reordered according to sample names

R/internal_utils.R
@@ -91,7 +91,7 @@ i_match_label = function(x, i, full = FALSE, allow_empty_lookup = FALSE){
         }
     }
 
-    m = which(  x %in% i)
+    m = match(i, x) # return matches in the same order as i. allows to reorder samples
     u = which(! x %in% i)
     n = setdiff(i, x)
 

commit dd5e02d8445fe6494309d92c17cc61bd0a9dd705

fixed bug introduced in d595e62 where label matching subset always returned a single spectra

R/internal_utils.R
@@ -91,7 +91,11 @@ i_match_label = function(x, i, full = FALSE, allow_empty_lookup = FALSE){
         }
     }
 
-    m = match(i, x) # return matches in the same order as i. allows to reorder samples
+    ds = match(x, i)
+    dw = which(!is.na(ds))
+    ds = order(ds[dw])
+
+    m = dw[ds]
     u = which(! x %in% i)
     n = setdiff(i, x)
 