73623c5bfcb2c86064313022c92f4669a5b5807d
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-05-06 21:58:22 -0500

    added has_nir_dip function

--- R/utils.R
+++ R/utils.R
@@ -16,6 +16,30 @@ is_spectra = function(x){
 }
 
 
+#' Has a dip in the NIR region?
+#'
+#' \code{has_nir_dip} tests if spectra have a NIR dip
+#'
+#' @param x spectra object
+#' @return boolean vector
+#'
+#' @author Jose Eduardo Meireles
+#' @export
+has_nir_dip = function(x){
+
+    if( !is_spectra(x) ){
+        stop("Object must be of class spectra")
+    }
+
+    y = spectrolab::resample(x[ , wavelengths(x, 700, 800)], 700:800)
+    z = apply(y, 1, diff, differences = 2)
+    w = apply(z[30:65, ], 2, function(x){ any(x > 0.0) })
+
+    as.vector(w)
+}
+
+
+
 #' Wrap function to try to keep text
 #'
 #' Function operator that returns a function f that tries to keep text.
