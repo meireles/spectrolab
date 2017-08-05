0272e0121f04ffa831a26aaa3cfd89c3a140bf9e
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-08-05 16:04:42 -0500

    simulating jumps in spectra

--- data-raw/simulate_spectra.R
+++ data-raw/simulate_spectra.R
@@ -1,5 +1,6 @@
 library(PEcAnRTM)
 library(devtools)
+library(spectrolab)
 
 n = 50
 params = matrix( c(rnorm(n, 1.2, 0.05),
@@ -21,6 +22,12 @@ sampl_names    = sample(names, size = n, replace = TRUE)
 
 spec_matrix_example = cbind(species = sampl_names, refl)
 
+spec_with_jump               = as.spectra(spec_matrix_example)[1, ]
+spec_with_jump[ , 400:970]   = spec_with_jump[ , 400:970]   * 1.06
+spec_with_jump[ , 1910:2500] = spec_with_jump[ , 1910:2500] * 1.5
+
 devtools::use_data(spec_matrix_example, overwrite = TRUE)
+devtools::use_data(spec_with_jump, overwrite = TRUE)
+
 #write.csv(data, "inst/extdata/spectra.csv", row.names = FALSE)
 
