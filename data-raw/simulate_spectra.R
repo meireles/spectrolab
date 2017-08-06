e714e40d98d968fa4d47251dd3428c807268e636
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-08-06 13:54:06 -0500

    new simulated sensor jump

--- data-raw/simulate_spectra.R
+++ data-raw/simulate_spectra.R
@@ -23,11 +23,8 @@ sampl_names    = sample(names, size = n, replace = TRUE)
 spec_matrix_example = cbind(species = sampl_names, refl)
 
 spec_with_jump               = as.spectra(spec_matrix_example)[1, ]
-spec_with_jump[ , 400:970]   = spec_with_jump[ , 400:970]   * 1.06
+spec_with_jump[ , 400:970]   = spec_with_jump[ , 400:970]   * 1.08
 spec_with_jump[ , 1910:2500] = spec_with_jump[ , 1910:2500] * 1.5
 
 devtools::use_data(spec_matrix_example, overwrite = TRUE)
 devtools::use_data(spec_with_jump, overwrite = TRUE)
-
-#write.csv(data, "inst/extdata/spectra.csv", row.names = FALSE)
-
