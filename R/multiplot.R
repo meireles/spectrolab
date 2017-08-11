58953f4e2a5d085f45bc6fa24470df1133529eab
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-08-11 15:12:27 -0500

    overhaul interactive plot feature

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,4 +1,5 @@
 devtools::use_package("shiny")
+devtools::use_package("grDevices")
 
 #' Interactive spectra plot
 #'
@@ -6,56 +7,104 @@ devtools::use_package("shiny")
 #' helpful for data inspection and large datasets.
 #'
 #' @param spec spectra object
+#' @param colpalette a color palette function, e.g. rainbow, terrain.colors, etc.
+#'                   or a function returned from `colorRampPalette`.
+#' @param ... Other arguments passed to plot
 #' @return interactive plot
 #'
 #' @examples
 #' \dontrun{
-#' # Create a reflectance matrix.
-#' rf = spec_matrix_example[, -1]
-#'
-#' # Create a vector with wavelength labels.
-#' wl = colnames(rf)
-#'
-#' # Sample with replacement to increase the number of spectra.
-#' rf = rf[sample(nrow(rf),300,replace = T),]
-#'
-#' # Create a vector with sample labels that match the reflectance matrix rows.
-#' sn = paste(rep("sp",300), 1:300, sep = "_")
-#'
-#' # Construct the spectra object using the `spectra` constructor
-#' spec = spectra(reflectance = rf, wavelengths = wl, names = sn)
+#' # Create a spectra object
+#' spec = as.spectra(spec_matrix_example)
 #'
 #' # Start interactive plot
 #' plot_interact(spec)
 #' }
 #'
 #' @importFrom shiny shinyApp numericInput actionButton verbatimTextOutput plotOutput renderPlot renderText
+#' @importFrom grDevices colorRampPalette rainbow heat.colors terrain.colors
 #'
-#' @author Anna K. Schweiger
+#' @author Anna K. Schweiger and Jose Eduardo Meireles
 #' @export
 
-plot_interact = function(spec) {
+plot_interact = function(spec,
+                         colpalette = grDevices::colorRampPalette(c("red", "orange", "blue", "purple")),
+                         ... ){
+
     if (!requireNamespace("shiny", quietly = TRUE)) {
         stop("Package shiny needed for this function to work. Please install it.",
              call. = FALSE)
     }
+
+    if( ! is.function(colpalette) ){
+        message("colpalette must be a function! Using the default palette.")
+        colpalette = grDevices::colorRampPalette(c("red", "orange", "blue", "purple"))
+    }
+
+    # Constants
+    n_max     = nrow(spec)
+    i_display = min(10, n_max)
+
+    # Begin shiny app
     shiny::shinyApp(
         ui = shiny::fluidPage(
-            shiny::numericInput(inputId = "num1", label = "number of spectra",
-                                value = 20, min = 1, width = "25%"),
-            shiny::actionButton("action2", label = "previous"),
-            shiny::actionButton("action", label = "next"),
+            shiny::numericInput(inputId = "n_display",
+                                label   = "number of spectra",
+                                value   = i_display,
+                                min     = 1,
+                                max     = n_max,
+                                width   = "20%"),
+            shiny::actionButton("go_back", label = "previous"),
+            shiny::actionButton("go_fwd", label = "next"),
             shiny::verbatimTextOutput("firstlast"),
             shiny::plotOutput("spectrum")
         ),
 
         server = function(input, output){
-            output$spectrum = shiny::renderPlot({
-                plot(spec[(1 : input$num1) + input$num1 * input$action - input$num1 * input$action2, ],
-                     col = 1 : 50)
+            # Initialize range variables
+            from  = shiny::reactiveVal(1)
+            to    = shiny::reactiveVal(1)
+
+            # Update `from` and `to` if next is pressed
+            shiny::observeEvent(input$go_fwd, {
+                # update from
+                old_from = from()
+                new_from = min(old_from + input$n_display, n_max)
+                from(new_from)
+
+                # update to
+                new_to   = min(from() + input$n_display - 1L, n_max)
+                to(new_to)
             })
-            output$firstlast = shiny::renderText({paste("spectra from", 1 + input$num1 * input$action - input$num1 * input$action2,
-                                                        "to", input$num1 + input$num1 * input$action - input$num1 * input$action2)})
+
+            # Update `from` and `to` if previous is pressed
+            shiny::observeEvent(input$go_back ,{
+                # update from
+                old_from = from()
+                new_from = max(old_from - input$n_display, 1L)
+                from(new_from)
+
+                # update to
+                new_to = min(from() + input$n_display - 1L, n_max)
+                to(new_to)
+            })
+
+            # Update `to` if n_display is changed
+            shiny::observeEvent(input$n_display ,{
+                new_to   = min(from() + input$n_display - 1L, n_max)
+                to(new_to)
+            })
+
+            # Plot spectra
+            output$spectrum = shiny::renderPlot({
+                s_range = seq(from(), to())
+                plot(spec[ s_range, ], col = colpalette(length(s_range)), ...)
+            })
+
+            # Plot text
+            output$firstlast = shiny::renderText({
+                paste("spectra: ", from(), "-", to(), "/", n_max, sep = "")
+            })
         }
     )
 }
