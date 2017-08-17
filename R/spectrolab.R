4e0600e848002b5137c88c232c2ffc1e1beaa510
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-08-17 15:47:20 -0500

    added welcome message when spectrolab initiates. shows version and citation

--- R/spectrolab.R
+++ R/spectrolab.R
@@ -1,10 +1,22 @@
-library("devtools")
 devtools::use_package("devtools")
+library("devtools")
+
+descrip            = readLines("DESCRIPTION")
+spectrolab_version = tolower(descrip[ grep("Version:", descrip) ])
+spectrolab_citation = format(citation("spectrolab"), style = "text")
+spectrolab_citation = gsub("_", "", spectrolab_citation)
 
 #' Spectrolab
 #'
-#' Class and methods for hyperspectral data
+#' Class and methods for hyperspectral data.
 #'
 #' @docType package
 #' @name spectrolab
 NULL
+
+.onAttach = function(libname, pkgname) {
+    packageStartupMessage("spectrolab ",
+                          spectrolab_version, "\n\n",
+                          "Please cite:\n",
+                          spectrolab_citation)
+}
