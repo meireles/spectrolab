e4231ee325663f5cca23a0048b925eee75219b20
Jose Eduardo Meireles <jemeireles@gmail.com>
2017-09-20 12:40:20 -0500

    internal spline smoothing now requires the parallel package

--- NEWS.md
+++ NEWS.md
@@ -3,6 +3,7 @@
 ## minor
 * fixed index duplication in the _match sensor_ vignette.
 * added CRAN installation instructions to readme.md
+* internal spline smoothing now requires the parallel package
 
 # spectrolab 0.0.2 (2017-09-14)
  
--- R/spectrolab.R
+++ R/spectrolab.R
@@ -1,8 +1,8 @@
 devtools::use_package("devtools")
-library("devtools")
+# library("devtools")
 
-descrip            = readLines("DESCRIPTION")
-spectrolab_version = tolower(descrip[ grep("Version:", descrip) ])
+descrip             = readLines("DESCRIPTION")
+spectrolab_version  = tolower(descrip[ grep("Version:", descrip) ])
 spectrolab_citation = format(citation("spectrolab"), style = "text")
 spectrolab_citation = gsub("_", "", spectrolab_citation)
 
