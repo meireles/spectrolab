240416189110d8c4130362737ed36fa50144567f
Jose Eduardo Meireles <jemeireles@gmail.com>
2020-07-03 21:14:00 -0400

    refactored normalize

--- R/transform_spectra.R
+++ R/transform_spectra.R
@@ -353,6 +353,7 @@ subset_by.spectra = function(x, by, n_min, n_max, random = TRUE){
 #' Vector normalize spectra
 #'
 #' \code{normalize} returns a spectra obj with vector normalized values
+#' Normalization value for each spectrum is computed as sqrt(sum(x_i^2))
 #'
 #' @param x spectra object. bands must be strictly increasing
 #' @param quiet boolean. Warn about change in y value units? Defaults to FALSE
@@ -388,14 +389,9 @@ normalize.spectra = function(x, quiet = FALSE, ...){
         }
     }
 
-    refl            = value(x)
-    refl_squared    = refl * refl
-    vec_ones        = rep.int(1L, ncol(refl_squared))
-    spec_sq_rowsum  = refl_squared %*% vec_ones
-    magnitudes      = as.vector(sqrt(spec_sq_rowsum))
-
-    # normalize and construct a `spectra` object
-    x[] = i_value(refl / magnitudes)
+    refl        = value(x)
+    magnitudes  = sqrt(apply(refl^2, 1, sum))
+    x[]         = refl / magnitudes
 
     # add a magnitute attribute to the`spectra` object
     meta(x, "normalization_magnitude") = magnitudes
