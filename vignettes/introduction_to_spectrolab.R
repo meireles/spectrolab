c69f50703ba144568cde91e30b7243e600c17302
Jose Eduardo Meireles <jemeireles@gmail.com>
2026-07-20 15:04:42 -0400

    revised and new vignettes

--- vignettes/introduction_to_spectrolab.R
+++ vignettes/introduction_to_spectrolab.R
@@ -1,12 +1,25 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+    collapse    = TRUE,
+    comment     = "#>",
+    fig.align   = "center",
+    fig.retina  = 2,
+    dpi         = 150
+)
+
+
 ## ----eval=FALSE---------------------------------------------------------------
 # install.packages("spectrolab")
+
 
 ## ----eval=FALSE---------------------------------------------------------------
 # library("devtools")
 # install_github("meireles/spectrolab")
 
+
 ## ----echo=TRUE, message=FALSE-------------------------------------------------
 library("spectrolab")
+
 
 ## ----eval=TRUE----------------------------------------------------------------
 # `dir_path` is the directory where our example datasets live
@@ -15,6 +28,7 @@ dir_path = system.file("extdata/Acer_example", package = "spectrolab")
 # Read spectra from .sig files inside the "extdata/Acer_example" folder
 acer_spectra = read_spectra(path = dir_path)
 
+
 ## ----eval=TRUE----------------------------------------------------------------
 # Reading the target's radiance
 acer_spectra_rad = read_spectra(path = dir_path, format = "sig", type = "target_radiance")
@@ -22,9 +36,11 @@ acer_spectra_rad = read_spectra(path = dir_path, format = "sig", type = "target_
 # And the white reference's radiance
 acer_white_ref = read_spectra(path = dir_path, type = "reference_radiance")
 
+
 ## ----eval=TRUE----------------------------------------------------------------
 # Use the `exclude_if_matches` argument to excluded flagged files
 acer_spectra = read_spectra(path = dir_path, exclude_if_matches = c("BAD","WR"))
+
 
 ## ----eval=TRUE, message=FALSE-------------------------------------------------
 # Use the `exclude_if_matches` argument to excluded flagged files
@@ -37,11 +53,12 @@ acer_spectra_with_meta = read_spectra(path = dir_path,
 
 meta(acer_spectra_with_meta)[1:3, 2:6]
 
+
 ## ----eval=TRUE----------------------------------------------------------------
 dir_path = system.file("extdata/spec_matrix_meta.csv", package = "spectrolab")
 
 # Read data from the CSV file. If you don't use `check.names` = FALSE when reading
-# the csv, R will usually add a letter to the column names (e.g. 'X650') which will 
+# the csv, R will usually add a letter to the column names (e.g. 'X650') which will
 # cause problems when converting the matrix to spectra.
 spec_csv = read.csv(dir_path, check.names = FALSE)
 
@@ -51,6 +68,7 @@ achillea_spec = as_spectra(spec_csv, name_idx = 3, meta_idxs = c(1,2) )
 # And now you have a spectra object with sample names and metadata...
 achillea_spec
 
+
 ## ----eval=TRUE----------------------------------------------------------------
 # Simply print the object
 acer_spectra
@@ -58,11 +76,12 @@ acer_spectra
 # Get the dataset's dimensions
 dim(acer_spectra)
 
+
 ## ----eval=TRUE----------------------------------------------------------------
 # Vector of all sample names. Note: Duplicated sample names are permitted
 n = names(achillea_spec)
 
-# Vector of bands
+# Vector of bands. Note: Duplicated band labels are permitted too (see "Gotchas" below)
 w = bands(achillea_spec)
 
 # value matrix
@@ -70,6 +89,7 @@ r = value(achillea_spec)
 
 # Metadata. Use simplify = TRUE to get a vector instead of a data.frame
 m = meta(achillea_spec, "ssp", simplify = TRUE)
+
 
 ## ----eval = TRUE--------------------------------------------------------------
 # Subset band regions. Here we know that bands are integers (e.g. 400, 401, ...)
@@ -80,19 +100,24 @@ spec_sub_vis = achillea_spec[ , 400:700 ]
 spec_sub_byname = achillea_spec["ACHMI_7", ]
 spec_sub_byidx  = achillea_spec[ 1:3, ]
 
+
 ## ----eval=TRUE----------------------------------------------------------------
 acer_spectra_trim = acer_spectra[ , bands(acer_spectra, 400, 2400) ]
+
 
 ## ----eval=TRUE----------------------------------------------------------------
 # Subsetting samples by indexes works and so does subsetting bands by numerics or characters.
 spec_sub_byidx[1, "405"] == spec_sub_byidx[1, 405]
 
-## ----eval=T, error=T----------------------------------------------------------
-# Something that is obvioulsy an index, like using 2 instead of 401 (the 2nd band 
-# in our case), will fail.
-spec_sub_byidx[ , 2]
 
-`Error in i_match_ij_spectra(this = this, i = i, j = j) : band subscript out of bounds. Use band labels instead of raw indices.`
+## ----eval=TRUE, error=TRUE----------------------------------------------------
+try({
+# Something that is obviously a column position, like 2 instead of 401
+# (the actual label of the 2nd band in our data), will fail with an
+# informative error rather than silently returning the wrong band.
+spec_sub_byidx[ , 2]
+})
+
 
 ## ----fig.height=2.5, fig.width=7, eval=TRUE-----------------------------------
 # Simple spectra plot
@@ -112,7 +137,8 @@ plot_regions(achillea_spec, regions = default_spec_regions(), add = TRUE)
 
 par(oldpar)
 
-## ----eval=T-------------------------------------------------------------------
+
+## ----eval=TRUE----------------------------------------------------------------
 spec_new = achillea_spec
 
 # Replace names with a lowercase version
@@ -121,7 +147,8 @@ names(spec_new) = tolower(names(achillea_spec))
 # Check the results
 names(spec_new)[1:5]
 
-## ----fig.height=3, fig.width=4, fig.align="center", eval=T--------------------
+
+## ----fig.height=3, fig.width=4, fig.align="center", eval=TRUE-----------------
 # Scale value by 0.75
 spec_new = spec_new * 0.75
 
@@ -129,12 +156,22 @@ spec_new = spec_new * 0.75
 plot(achillea_spec, col = "blue", lwd = 0.75, cex.axis = 0.75)
 plot(spec_new, col = "orange", lwd = 0.75, add = TRUE)
 
+
 ## ----eval = TRUE--------------------------------------------------------------
 ## Adding metadata to a spectra object: a dummy N content
 n_content = rnorm(n = nrow(achillea_spec), mean = 2, sd = 0.5)
 meta(achillea_spec, label = "N_percent") = n_content
 
-## ----eval=T-------------------------------------------------------------------
+
+## ----eval = TRUE, message = FALSE---------------------------------------------
+# Average reflectance per species (metadata columns get FUN applied too, when
+# possible -- non-numeric metadata, like species names, becomes NA rather
+# than erroring, with a message)
+spec_species_mean = aggregate(achillea_spec, by = names(achillea_spec), FUN = mean)
+dim(spec_species_mean)
+
+
+## ----eval=TRUE----------------------------------------------------------------
 # Make a matrix from a `spectra` object
 spec_as_mat = as.matrix(achillea_spec, fix_names = "none")
 spec_as_mat[1:4, 1:3]
@@ -142,4 +179,21 @@ spec_as_mat[1:4, 1:3]
 # Make a matrix from a `spectra` object
 spec_as_df = as.data.frame(achillea_spec, fix_names = "none", metadata = TRUE)
 spec_as_df[1:4, 1:5]
+
+
+## ----eval = TRUE--------------------------------------------------------------
+dup = achillea_spec
+bands(dup)[2] = bands(dup)[1]   # force a duplicate band label
+
+# Selecting the duplicated label returns both matching columns -- not an error
+dup[1, bands(dup)[1]]
+
+
+## ----eval = TRUE--------------------------------------------------------------
+s1 = s2 = achillea_spec
+meta(s1, "source") = "s1"
+meta(s2, "source") = "s2"
+
+r = s1 + s2
+meta(r, "source", simplify = TRUE)[1:3]   # "s1", not "s2" -- s2's metadata is gone
 
---
+++ vignettes/preprocessing.R
@@ -0,0 +1,88 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+    collapse   = TRUE,
+    comment    = "#>",
+    fig.align  = "center",
+    fig.retina = 2,
+    dpi        = 150
+)
+
+
+## ----message = FALSE----------------------------------------------------------
+library(spectrolab)
+
+spec = as_spectra(spec_matrix_example, name_idx = 1)
+one  = spec[1, ]   # a single spectrum, used for the "before/after" plots below
+
+
+## ----fig.height=3.5, fig.width=7----------------------------------------------
+s_gauss = smooth(one, method = "gaussian")
+s_sg    = suppressMessages(smooth(one, method = "sgolay"))
+
+oldpar = par(no.readonly = TRUE)
+par(mfrow = c(1, 2))
+
+plot(one, lwd = 1, col = "grey60", main = "Gaussian (default)")
+plot(s_gauss, add = TRUE, col = "firebrick", lwd = 1)
+
+plot(one, lwd = 1, col = "grey60", main = "Savitzky-Golay")
+plot(s_sg, add = TRUE, col = "steelblue", lwd = 1)
+
+par(oldpar)
+
+
+## ----fig.height=3.5, fig.width=7----------------------------------------------
+s_narrow = smooth_sgolay(one, p = 3, n = 5)    # default-sized window
+s_wide   = smooth_sgolay(one, p = 3, n = 51)   # a much wider window
+
+oldpar = par(no.readonly = TRUE)
+par(mfrow = c(1, 2))
+
+plot(one, lwd = 1, col = "grey60", main = "n = 5 (default-sized)")
+plot(s_narrow, add = TRUE, col = "steelblue", lwd = 1)
+
+plot(one, lwd = 1, col = "grey60", main = "n = 51")
+plot(s_wide, add = TRUE, col = "steelblue", lwd = 1)
+
+par(oldpar)
+
+
+## ----fig.height=3.5, fig.width=7----------------------------------------------
+d1 = suppressMessages(deriv_spectra(one, order = 1))
+d2 = suppressMessages(deriv_spectra(one, order = 2))
+
+oldpar = par(no.readonly = TRUE)
+par(mfrow = c(1, 2))
+
+plot(d1, main = "1st derivative", col = "darkgreen", ylab = "dReflectance/dWavelength")
+abline(h = 0, col = "grey", lty = 2)
+
+plot(d2, main = "2nd derivative", col = "purple", ylab = "d2Reflectance/dWavelength2")
+abline(h = 0, col = "grey", lty = 2)
+
+par(oldpar)
+
+
+## ----fig.height=3.5, fig.width=7----------------------------------------------
+one_cr = continuum_removal(one)
+
+# Compute the hull the same way continuum_removal() does internally, purely
+# to visualize it -- this is NOT required to use continuum_removal() itself.
+w = bands(one)
+r = as.numeric(value(one))
+h = grDevices::chull(w, r)
+chord = r[[1]] + (r[[length(r)]] - r[[1]]) * (w[h] - w[[1]]) / (w[[length(w)]] - w[[1]])
+h = sort(h[r[h] >= chord - sqrt(.Machine$double.eps)])
+hull = stats::approx(w[h], r[h], xout = w)$y
+
+oldpar = par(no.readonly = TRUE)
+par(mfrow = c(1, 2))
+
+plot(one, lwd = 1, col = "grey30", main = "Spectrum and its continuum")
+lines(w, hull, col = "firebrick", lwd = 1.5)
+
+plot(one_cr, lwd = 1, col = "darkorange", main = "Continuum-removed")
+abline(h = 1, col = "grey", lty = 2)
+
+par(oldpar)
+
